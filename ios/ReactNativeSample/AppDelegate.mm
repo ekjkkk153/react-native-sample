0bb756549aad71c6373244472071a813cfd37fff
ekjkkk153 <ekjkkk153@daum.net>
2024-11-27 10:04:25 +0900

    expo prebuild -p ios --clean 처리 및 소스 재작업

--- ios/ReactNativeSample/AppDelegate.mm
+++ ios/ReactNativeSample/AppDelegate.mm
@@ -1,33 +1,3 @@
-//#import "AppDelegate.h"
-//
-//#import <React/RCTBundleURLProvider.h>
-//#import <React/RCTLinkingManager.h>
-//#import <React/RCTConvert.h>
-//
-//@implementation AppDelegate
-//
-//- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-//{
-//  self.moduleName = @"react-native-sample";
-//  // You can add your custom initial props in the dictionary below.
-//  // They will be passed down to the ViewController used by React Native.
-//  self.initialProps = @{};
-//  
-//  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-//}
-// 
-//- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-//{
-//  return [self getBundleURL];
-//}
-// 
-//- (NSURL *)getBundleURL
-//{
-//#if DEBUG
-//  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-//#endif
-//}
-// @end
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
@@ -48,10 +18,10 @@
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self bundleURL];
+  return [self getBundleURL];
 }
 
-- (NSURL *)bundleURL
+- (NSURL *)getBundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
